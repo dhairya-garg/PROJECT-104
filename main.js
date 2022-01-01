41e29cc4deab137db8c7509eda1d85579f0e71e0
dhairya-garg <82100537+dhairya-garg@users.noreply.github.com>
2022-01-01 21:13:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+// https://teachablemachine.withgoogle.com/models/UBdJBAHQm/
+
+Webcam.set({
+    width:300,
+  height:300,
+  image_format:"png",
+png_quaility:90,
+})
+
+camera=document.getElementById("camera")
+
+Webcam.attach("#camera")
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+     document.getElementById("result").innerHTML="<img id='captured_image' src='"+data_uri+"'>"   
+    })
+}
+console.log("ml5 version:",ml5.version)
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/UBdJBAHQm/model.json",modelloaded)
